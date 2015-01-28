1f7a927a0dd6f807bf27910b90a2d271e86f2cd7
venus-show <418108124@qq.com>
2015-01-28 15:22:52 +0800

    春运高速拥堵补贴

---
+++ subsidy_list.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once("layout/meta.php"); ?>
+    <title>春运高速拥堵补贴</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="Browser">
+    
+   <?php include_once("layout/nav.php"); ?>
+
+    <article class="article">
+        <div class="subsidy_list">
+            <ul class="list_cons">
+                <li class="item">
+                    <div class="left_tit">
+                        <div class="time">
+                            <p>始：2015-01-06  12:16:56</p>
+                            <p>终：2015-01-06  15:32:20</p>
+                        </div>
+                        <div class="site">地点：京沪高速南京路段</div>
+                    </div>
+                    <div class="right_tit">
+                        10L
+                    </div>
+                </li>
+                 <li class="item">
+                    <div class="left_tit">
+                        <div class="time">
+                            <p>始：2015-01-06  12:16:56</p>
+                            <p>终：2015-01-06  15:32:20</p>
+                        </div>
+                        <div class="site">地点：京沪高速南京路段</div>
+                    </div>
+                    <div class="right_tit">
+                        10L
+                    </div>
+                </li>
+                 <li class="item">
+                    <div class="left_tit">
+                        <div class="time">
+                            <p>始：2015-01-06  12:16:56</p>
+                            <p>终：2015-01-06  15:32:20</p>
+                        </div>
+                        <div class="site">地点：京沪高速南京路段</div>
+                    </div>
+                    <div class="right_tit">
+                        10L
+                    </div>
+                </li>
+                 <li class="item">
+                    <div class="left_tit">
+                        <div class="time">
+                            <p>始：2015-01-06  12:16:56</p>
+                            <p>终：2015-01-06  15:32:20</p>
+                        </div>
+                        <div class="site">地点：京沪高速南京路段</div>
+                    </div>
+                    <div class="right_tit">
+                        10L
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </article>
+    <script type="text/javascript" src="js/zepto.js"></script>
+    <script type="text/javascript" src="js/extend.js"></script>
+    <script type="text/javascript" src="js/common.js"></script>
+</body>
+</html>
