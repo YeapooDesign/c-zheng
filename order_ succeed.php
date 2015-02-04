a344fc2cbe9f22e0e7ccae056ff4b5e1b797deaf
venus-show <418108124@qq.com>
2015-02-04 10:53:23 +0800

    新增 预约成功页面

---
+++ order_ succeed.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once("layout/meta.php"); ?>
+    <title>预约成功</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="Browser">
+    
+   <?php include_once("layout/nav.php"); ?>
+    
+    <article class="article">
+        <div class="order_succeed mt15">
+            <ul class="tit_con">
+                <li class="item width_line">
+                    <div class="num">1.</div>
+                    <div class="cont">
+                        <p>恭喜您已经预约安装成功，安装门店信息为：</p>
+                        <p>安装时间：2015年2月1日</p>
+                        <p>安装门店：广粤路60号</p>
+                    </div>
+                </li>
+                <li class="item">
+                    <div class="num">2.</div>
+                    <div class="cont">
+                    <p>如果在安装中或使用中有任何问题</p>
+                    <p>请访问车挣官网：www.ichezheng.com</p>
+                    <p>或拨打400-034-5858</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </article>
+    <script type="text/javascript" src="js/zepto.js"></script>
+    <script type="text/javascript" src="js/extend.js"></script>
+    <script type="text/javascript" src="js/common.js"></script>
+</body>
+</html>
