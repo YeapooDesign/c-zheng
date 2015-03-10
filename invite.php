8cf7a06d42c65c36cea9d530edbafed880e60f1c
venus-show <418108124@qq.com>
2015-03-10 17:00:46 +0800

    新增底部立即预约页面

---
+++ invite.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once("layout/meta.php"); ?>
+    <title>我已经(用)车挣(钱)了,你还在等什么? —智能汽车设备免费送！</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="Browser" style="background:#F6F6F6;">
+    <div class="invite_cons">
+        <div class="img">
+            <img src="images/shareinfo.png" alt="">
+        </div>
+        <div class="invite_bottom">
+            <div class="invite_btn">立即预约</div>
+        </div>
+    </div>
+</body>
+</html>
