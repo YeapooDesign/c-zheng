a06d6feb1cd0e184a8665da9a66063486ffed3d8
venus-show <418108124@qq.com>
2015-03-27 10:04:50 +0800

    系统维护中小页面

---
+++ maintain.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once("layout/meta.php"); ?>
+    <title>系统维护中</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="Browser" style="background:#fff">
+    
+   <?php include_once("layout/nav.php"); ?>
+
+    <article class="article" >
+        <div class="maintain">
+           <div class="top_img">
+               <img src="images/maintain.png" alt="">
+           </div>
+           <h1>系统正在维护中...</h1>
+        </div>
+    </article>
+    <script type="text/javascript" src="js/zepto.js"></script>
+    <script type="text/javascript" src="js/extend.js"></script>
+    <script type="text/javascript" src="js/common.js"></script>
+</body>
+</html>
