9279a13e38e82f6eeb67ab8fe91836c764c0c0eb
venus-show <418108124@qq.com>
2015-03-27 17:43:32 +0800

    清明补贴页面文案修改

--- QM_subsidy.php
+++ QM_subsidy.php
@@ -28,22 +28,20 @@
             <ul class="tit_con">
                 <li class="item width_line">
                     <div class="title">规则：</div>
-                    <p>1、4月1日零点至4月7日24点；</p>
+                    <p>1、<span>4月1日</span>零点至4月7日24点；</p>
                     <p>2、国内高速路段上（休息区、加油站除）；</p>
                     <p>3、点火状态下；</p>
                     <p>4、连续2小时，累积行驶距离小于等于20公里。</p>
                 </li>
                 <li class="item width_line">
-                    <div class="title">如果满足如上条件，则：</div>
-                    <p>1、车挣补贴10升95#汽油/次，每天最多1次</p>
-                    <p>&nbsp&nbsp&nbsp&nbsp&nbsp累积最高2次</p>
+                    <div class="title3">如果满足如上条件，则车挣补贴10升95#汽油/次，每天最多1次，累积最高2次。</div>
                 </li>
                 <li class="item">
                     <div class="title2">备注：</div>
                     <p>1、本次活动不收费，但需要注册参与。</p>
                     <p>&nbsp&nbsp&nbsp&nbsp&nbsp仅限注册成功后的行程，注册前的行程不计入。</p>
                     <p>2、补贴汽油按照4月10日的官方价格折算为挣点，</p>
-                    <p>&nbsp&nbsp&nbsp&nbsp&nbsp4月13日前发放。</p>
+                    <p>&nbsp&nbsp&nbsp&nbsp&nbsp4月13日发放。</p>
                     <p>3、以服务器接收到的数据为准。</p>
                 </li>
             </ul>
