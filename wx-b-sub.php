04e270e4981488ecd699f1f1fc1fee658760425c
venus-show <418108124@qq.com>
2015-06-05 11:55:06 +0800

    新增页面

---
+++ wx-b-sub.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once("layout/meta.php"); ?>
+    <title>领奖</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body style="background:#070707">
+    <article class="article">
+        <div class="wx-b-sub">
+            <div class="img">
+                <img src="images/black-wx-partner.jpg" alt="">
+            </div>
+            <div class="wx-b-btn"><a href="#">立即预约</a></div>
+        </div>
+    </article>
+</body>
+</html>
