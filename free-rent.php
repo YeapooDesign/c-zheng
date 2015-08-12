e1123abe76f299f76dd7ca9e9d667f5b403820e5
venus-show <418108124@qq.com>
2015-08-12 13:24:53 +0800

    更新 新增一个页面和修改活动页面

---
+++ free-rent.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" id="html">
+<head>
+    <?php include_once("layout/meta.php"); ?>
+    <title>小挣数据中心</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body style="background:#0E0D12">
+        <div class="free-rent">
+            <div class="bg">
+                <img src="images/free-rent.jpg" alt="">
+            </div>
+            <a href="" class="btn btn-t"></a>
+            <a href="" class="btn btn-b"></a>
+        </div>
+    <script type="text/javascript" src="js/zepto.js"></script>
+    <script type="text/javascript" src="js/extend.js"></script>
+    <script type="text/javascript" src="js/common.js"></script>
+</body>
+</html>
